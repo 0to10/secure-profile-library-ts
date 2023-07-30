c8ca4a0d48007c58740ac7429ece92ae8b499c1b
0TO10 <121996729+0to10-dev@users.noreply.github.com>
2023-07-30 18:10:49 +0200

    Added HashAlgorithms and SignAlgorithms enums.

---
+++ src/HashAlgorithms.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * HashAlgorithms
+ *
+ * @copyright Copyright (c) 2023 0TO10 B.V. <https://0to10.nl>
+ * @license MIT
+ */
+export enum HashAlgorithms {
+    SHA_512 = 'SHA-512',
+}
---
+++ src/SignAlgorithms.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * SignAlgorithms
+ *
+ * @copyright Copyright (c) 2023 0TO10 B.V. <https://0to10.nl>
+ * @license MIT
+ */
+export enum SignAlgorithms {
+    RSA_SSA_PKCS1_V1_5 = 'RSASSA-PKCS1-V1_5',
+}
