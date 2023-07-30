669bb225ecce6a3fc1e0862d1b4d6925db355a73
0TO10 <121996729+0to10-dev@users.noreply.github.com>
2023-07-30 18:18:24 +0200

    Added AttributeType and CertificateExtension types in OID folder.

---
+++ src/OID/AttributeType.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * AttributeType
+ *
+ * @copyright Copyright (c) 2023 0TO10 B.V. <https://0to10.nl>
+ * @license MIT
+ */
+export enum AttributeType {
+    COMMON_NAME = '2.5.4.3',
+    COUNTRY_NAME = '2.5.4.6',
+}
---
+++ src/OID/CertificateExtension.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * CertificateExtension
+ *
+ * @copyright Copyright (c) 2023 0TO10 B.V. <https://0to10.nl>
+ * @license MIT
+ */
+export enum CertificateExtension {
+    KEY_USAGE = '2.5.29.15',
+    BASIC_CONSTRAINTS = '2.5.29.19',
+}
