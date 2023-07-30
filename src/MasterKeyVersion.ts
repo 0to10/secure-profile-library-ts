a1c135e41f79f0ad1c8d2a3f63b9dd67399e6a8f
0TO10 <121996729+0to10-dev@users.noreply.github.com>
2023-07-31 01:57:29 +0200

    Added MasterKeyVersion type.

---
+++ src/MasterKeyVersion.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * MasterKeyVersion
+ *
+ * @copyright Copyright (c) 2023 0TO10 B.V. <https://0to10.nl>
+ * @license MIT
+ */
+export type MasterKeyVersion = {
+    number: [number, number];
+    algorithm: {
+        name: string;
+        iv_length: number;
+    };
+}
