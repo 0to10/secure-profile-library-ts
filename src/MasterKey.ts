248e68d83482e69fda9c6f70b3625d08196ccb0c
0TO10 <121996729+0to10-dev@users.noreply.github.com>
2023-07-31 01:58:25 +0200

    WIP: Added MasterKey class, untested.

---
+++ src/MasterKey.ts
@@ -0,0 +1,101 @@
+'use strict';
+
+import {Cryptography} from './Cryptography';
+import {MasterKeyVersion} from './MasterKeyVersion';
+
+const DEFAULT_VERSION: MasterKeyVersion = {
+    number: [0, 0],
+    algorithm: {
+        name: 'AES-GCM',
+        iv_length: 96,
+    },
+};
+
+/**
+ * MasterKey
+ *
+ * @copyright Copyright (c) 2023 0TO10 B.V. <https://0to10.nl>
+ * @license MIT
+ */
+export class MasterKey {
+
+    private readonly crypto: SubtleCrypto;
+
+    constructor(
+        private readonly key: CryptoKey,
+        private readonly versions: Array<MasterKeyVersion> = [DEFAULT_VERSION],
+    ) {
+        this.crypto = Cryptography.getEngine();
+    }
+
+    public encrypt(
+        data: ArrayBuffer,
+        version: MasterKeyVersion = DEFAULT_VERSION,
+    ): Promise<ArrayBuffer> {
+        const VERSION_LENGTH: number = version.number.length;
+
+        if (VERSION_LENGTH > 255) {
+            throw new Error('Unable to use given version, as the number exceeds 255 bits');
+        }
+
+        const iv: ArrayBuffer = Cryptography.randomBytes(version.algorithm.iv_length);
+
+        const params: AesGcmParams = {
+            name: version.algorithm.name,
+            iv,
+        };
+
+        const promise: Promise<ArrayBuffer> = this.crypto.encrypt(params, this.key, data);
+
+        const DATA_OFFSET: number = 1 + VERSION_LENGTH;
+
+        return promise.then((encrypted: ArrayBuffer): ArrayBuffer => {
+            const result: Uint8Array = new Uint8Array(DATA_OFFSET + iv.byteLength + encrypted.byteLength);
+            result.set([version.number.length], 0);
+            result.set(version.number, 1);
+            result.set(new Uint8Array(iv), DATA_OFFSET);
+            result.set(new Uint8Array(encrypted), DATA_OFFSET + iv.byteLength);
+
+            return result;
+        });
+    }
+
+    public decrypt(
+        encrypted: ArrayBuffer,
+    ): Promise<ArrayBuffer> {
+        const version: MasterKeyVersion = this.getVersionFromEncryptedData(encrypted);
+
+        // Determine the offset of where the actual encrypted data starts:
+        // - 1 byte for storing the version
+        // - with the stored length
+        const DATA_OFFSET: number = 1 + encrypted[0];
+
+        const iv: ArrayBuffer = encrypted.slice(DATA_OFFSET, DATA_OFFSET + version.algorithm.iv_length);
+
+        const params: AesGcmParams = {
+            name: version.algorithm.name,
+            iv,
+        };
+
+        const data: ArrayBuffer = encrypted.slice(DATA_OFFSET + version.algorithm.iv_length);
+
+        return this.crypto.decrypt(params, this.key, data);
+    }
+
+    private getVersionFromEncryptedData(encrypted: ArrayBuffer): MasterKeyVersion {
+        let versionNumber: number = 0;
+        for (let i = 1; i <= encrypted[0]; i++) {
+            versionNumber |= encrypted[i] << (i * 8);
+        }
+
+        return this.versions.find(version => {
+            let currentVersionNumber: number = 0;
+            for (let i = 0, l = version.number.length; i < l; i++) {
+                currentVersionNumber |= version.number[i] << (i * 8);
+            }
+
+            return versionNumber === currentVersionNumber;
+        });
+    }
+
+}
