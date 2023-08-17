aa326ba58cd736b401528e5cfb1278d092916d75
0TO10 <121996729+0to10-dev@users.noreply.github.com>
2023-08-17 15:03:18 +0200

    Added CryptoKeyPairMap type.

---
+++ src/Account/CryptoKeyPairMap.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * CryptoKeyPairMap
+ *
+ * @copyright Copyright (c) 2023 0TO10 B.V. <https://0to10.nl>
+ * @license MIT
+ */
+export type CryptoKeyPairMap = {
+    [key: string]: CryptoKeyPair | undefined;
+}
