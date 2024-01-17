e0b7ee39147ec30b67490a30548c69a82befb30d
0TO10 <121996729+0to10-dev@users.noreply.github.com>
2024-01-17 18:37:25 +0100

    Added CryptoParameters type.

---
+++ src/CryptoParameters.type.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * CryptoParameters
+ *
+ * @copyright Copyright (c) 2024 0TO10 B.V. <https://0to10.nl>
+ * @license MIT
+ */
+export type CryptoParameters = Algorithm | RsaOaepParams | AesCtrParams | AesCbcParams | AesGcmParams | string;
