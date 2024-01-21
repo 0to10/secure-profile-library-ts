fd57b3ca8474c5e649d83d0d2c23fd88dd1d302f
0TO10 <121996729+0to10-dev@users.noreply.github.com>
2024-01-21 15:02:52 +0100

    Updated the tsconfig.json file, added index.ts file and set package version to "1.0.0".

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+export {CryptoParameters} from './CryptoParameters.type';
+export {CryptoVersions} from './CryptoVersions';
+export {Data} from './Account/Data';
+export {EncryptedProfile} from './Account/Profile/EncryptedProfile';
+export {MasterKey} from './MasterKey';
+export {ProfileFactory} from './Account/Profile/ProfileFactory';
+export {RandomStringGenerator} from './RandomStringGenerator';
+export {RoamingProfile} from './Account/Profile/RoamingProfile';
