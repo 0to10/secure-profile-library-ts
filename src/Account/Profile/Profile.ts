508217a6dca468d541a6e629d92045275bd430d7
0TO10 <121996729+0to10-dev@users.noreply.github.com>
2024-02-01 23:47:47 +0100

    Updated the abstract Profile class to use the KeyDerivation class.

--- src/Account/Profile/Profile.ts
+++ src/Account/Profile/Profile.ts
@@ -1,14 +1,12 @@
 'use strict';
 
 import {CryptoKey} from '@peculiar/webcrypto';
-import {scrypt} from 'scrypt-js';
-import unorm from 'unorm';
 
 import {Cryptography} from '../../Cryptography';
+import {KeyDerivation} from '../../Cryptography/KeyDerivation';
 import {MasterKey} from '../../MasterKey';
 
 const crypto: SubtleCrypto = Cryptography.getEngine();
-const textEncoder: TextEncoder = new TextEncoder();
 
 const MASTER_KEY_BITS: number = 256;
 
@@ -29,20 +27,10 @@ export abstract class Profile {
     public async deriveMasterKey(
         password: string,
     ): Promise<MasterKey> {
-        // The CPU/memory cost; increasing this increases the overall difficulty
-        const N: number = 32768;
-        // The block size; increasing this increases the dependency on memory latency and bandwidth
-        const r: number = 8;
-        // The parallelization cost; increasing this increases the dependency on multiprocessing
-        const p: number = 1;
-
-        const keyData: Uint8Array = await scrypt(
-            this.encoder.encode(unorm.nfkc(password)),
+        const keyData: Uint8Array = await KeyDerivation.fromPassword(
+            password,
             this.masterSalt,
-            N,
-            r,
-            p,
-            MASTER_KEY_BITS / 8
+            MASTER_KEY_BITS / 8,
         );
 
         const algorithm: AesKeyGenParams = {
@@ -60,10 +48,6 @@ export abstract class Profile {
 
     protected get crypto(): SubtleCrypto {
         return crypto;
-    }
-
-    protected get encoder(): TextEncoder {
-        return textEncoder;
     }
 
 }
